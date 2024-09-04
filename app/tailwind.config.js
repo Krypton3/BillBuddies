45b7ae1ddc059b342cf1d9a0cc3f7d028e5ee2e3
H M Mahedi Hasan <jisan2723@gmail.com>
2024-09-03 20:41:11 -0400

    Add nextui support to the app

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,11 +1,16 @@
+// tailwind.config.js
+const {nextui} = require("@nextui-org/react");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    // ...
+    // make sure it's pointing to the ROOT node_module
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  darkMode: "class",
+  plugins: [nextui()],
+};
